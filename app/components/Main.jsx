d1f7fb80a47c19ef3048414374eb65ce28c2e836
Bhavarsh Singh <bhavarshs@gmail.com>
2017-03-21 16:28:13 +0530

    Add countdown functionality

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -11,7 +11,7 @@ var Main = (props) => {
          <div className='row'>
 
              <div className='column small-centered medium-6 large-4'>
-                <p> Main.jsx Rendered </p>
+         
                    {props.children}
                </div>
            </div>
--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -1,55 +1,55 @@
-var expect = require('expect');
-
 var React = require('react');
 
 var ReactDOM = require('react-dom');
 
 
-var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+
 
 var $ = require('jQuery');
 
 
-var CountdownForm = require('CountdownForm');
+var TestUtils = require('react-addons-test-utils');
 
 
 
-describe('CountdownForm', ()=>{
+
+var Countdown = require('Countdown');
+
+
+describe('Countdown', ()=>{
 
     it('should exist', ()=>{
-
-        expect(CountdownForm).toExist();
-
-    });
-
-    it('should call onSetCountdown if valid seconds entered', ()=>{
-        var spy = expect.createSpy();
-
-        var countdownForm = TestUtils.renderIntoDocument(<CountdownForm  onSetCountdown={spy} />);
-
-        var $el = $(ReactDOM.findDOMNode(countdownForm));
-
-        countdownForm.refs.seconds.value = '109';
-
-
-        TestUtils.Simulate.submit($el.find('form')[0]);
-
-        expect(spy).toHaveBeenCalledWith(109);
+        expect(Countdown).toExist();
     })
 
+    describe('handleSetCountdown', ()=>{
+        it('should set state to started and countdown', (done)=>{
+            var countdown = TestUtils.renderIntoDocument(<Countdown />);
 
-    it('should not call onSetCountdown if valid seconds not entered', ()=>{
-        var spy = expect.createSpy();
-
-        var countdownForm = TestUtils.renderIntoDocument(<CountdownForm  onSetCountdown={spy} />);
-
-        var $el = $(ReactDOM.findDOMNode(countdownForm));
-
-        countdownForm.refs.seconds.value = '12b';
+            countdown.handleSetCountdown(10);
 
 
-        TestUtils.Simulate.submit($el.find('form')[0]);
+            expect(countdown.state.count).toBe(10);
 
-        expect(spy).toNotHaveBeenCalled();
+            expect(countdown.state.countdownStatus).toBe('started');
+
+            setTimeout(()=> {
+                expect(countdown.state.count).toBe(9);
+                done();
+            }, 1001);
+        })
+
+
+           it('should never set count less than zero', (done)=>{
+            var countdown = TestUtils.renderIntoDocument(<Countdown />);
+
+            countdown.handleSetCountdown(1);
+
+            setTimeout(()=> {
+                expect(countdown.state.count).toBe(0);
+                done();
+            }, 3001);
+        })
     })
 })
